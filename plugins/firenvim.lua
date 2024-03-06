c76268c89210bbe73ec6ad42a09d43ee2cbc5a6d
Ostrogoth <florian.roura@viaco.fr>
2024-03-06 18:19:07 +0100

    disable auto firenvim takeover

--- plugins/firenvim.lua
+++ plugins/firenvim.lua
@@ -6,5 +6,14 @@ return {
     lazy = not vim.g.started_by_firenvim,
     build = function()
         vim.fn["firenvim#install"](0)
+    end,
+    config = function()
+        vim.g.firenvim_config = {
+            localSettings = {
+                [".*"] = {
+                    takeover = "never"
+                }
+            }
+        }
     end
 }
