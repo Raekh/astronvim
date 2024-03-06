4483de94b024638abbc3522827c6cfd8c2f02519
Ostrogoth <florian.roura@viaco.fr>
2024-03-06 18:18:49 +0100

    conditional eslint/prettier lsp spawn

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -13,7 +13,30 @@ return {
     "jay-babu/mason-null-ls.nvim",
     -- overrides `require("mason-null-ls").setup(...)`
     opts = {
-      -- ensure_installed = { "prettier", "stylua" },
+      handlers = {
+        -- prettier
+        prettier = function()
+          require('null-ls').register(require('null-ls').builtins.formatting.prettier.with {
+            condition = function(utils)
+              return utils.root_has_file "package.json" and
+                  (utils.root_has_file ".prettierrc"
+                    or utils.root_has_file ".prettierrc.json"
+                    or utils.root_has_file ".prettierrc.js")
+            end
+          })
+        end,
+        -- eslint_d:
+        eslint_d = function()
+          require("null-ls").register(require("null-ls").builtins.diagnostics.eslint_d.with {
+            condition = function(utils)
+              return utils.root_has_file "package.json"
+                  and (utils.root_has_file ".eslintrc.json"
+                    or utils.root_has_file ".eslintrc.js"
+                    or utils.root_has_file ".eslintrc.mjs")
+            end,
+          })
+        end,
+      }
     },
   },
   {
