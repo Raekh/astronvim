c04184c2f39067bc67d6d9f07fc6c2507cff7128
Ostrogoth <florian.roura@viaco.fr>
2024-03-12 21:17:37 +0100

    suda + edit ts snippet

---
+++ plugins/suda.lua
@@ -0,0 +1,11 @@
+return {
+  'lambdalisue/suda.vim',
+  lazy = false,
+  config = function()
+    -- vim.g.suda_prompt = 'Pasuwodo: '
+    -- vim.g.suda_smart_edit = 1
+    vim.g['suda#prompt'] = 'Pasuwodo: '
+    vim.g['suda_smart_edit'] = 1
+  end,
+
+}
