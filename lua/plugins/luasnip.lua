8033f99b3de994e7637300f36034e6e6613ddcdb
Ostrogoth <florian.roura@viaco.fr>
2024-04-22 11:07:35 +0200

    snippets

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -3,6 +3,6 @@ return {
   lazy = false,
   config = function(plugin, opts)
     require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
-    require("luasnip.loaders.from_vscode").lazy_load { paths = { "./lua/user/snippets" } } -- load snippets paths
+    require("luasnip.loaders.from_vscode").lazy_load { paths = { vim.fn.stdpath "config" .. "/snippets" } } -- load snippets paths
   end,
 }
--- lua/plugins/typescript.lua
+++ lua/plugins/typescript.lua
@@ -1,10 +1,19 @@
-return  {
-  "jose-elias-alvarez/typescript.nvim",
-  config = true,
-  keys = {
-    { "<Leader>lu", "<CMD>TypescriptRemoveUnused<CR>", desc = "Typescript: Remove unused" },
-    { "<Leader>lm", "<CMD>TypescriptAddMissingImports<CR>", desc = "Typescript: Add missing imports" },
-    { "<Leader>lo", "<CMD>TypescriptOrganizeImports<CR>", desc = "Typescript: Organize imports" },
-  }
+return {
+  {
+    "jose-elias-alvarez/typescript.nvim",
+    config = true,
+    keys = {
+      { "<Leader>lu", "<CMD>TypescriptRemoveUnused<CR>", desc = "Typescript: Remove unused" },
+      { "<Leader>lm", "<CMD>TypescriptAddMissingImports<CR>", desc = "Typescript: Add missing imports" },
+      { "<Leader>lo", "<CMD>TypescriptOrganizeImports<CR>", desc = "Typescript: Organize imports" },
+    },
+  },
+  {
+    "dmmulroy/ts-error-translator.nvim",
+    config = true,
+  },
+  {
+    "dmmulroy/tsc.nvim",
+    config = true,
+  },
 }
-
